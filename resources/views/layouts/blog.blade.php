bc601e88f33380518bc6a278b492bf3a5f176096
JimJam117 <jamessparrow101@gmail.com>
2020-03-02 08:20:44 +0000

    Moved styles to styles.css

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -6,7 +6,7 @@
         @csrf
         <button aria-label="Search" type="submit" class="searchButton"><i class="fa fa-search" aria-hidden="true"></i></button>
         <input type="text" name="query" class="form-control searchInput" id="search"
-            placeholder="Search..." required>
+            placeholder="Search..." required autocomplete="off">
         <label style="display: none;" for="search">Search</label>
     </form>
 </div>
