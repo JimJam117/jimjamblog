ac20f934dfaf88f422a9f93561a5ef00f4b6f794
JimJam117 <jamessparrow101@gmail.com>
2020-10-02 17:07:31 +0100

    Simplfied the Home Page

--- resources/js/components/Home.jsx
+++ resources/js/components/Home.jsx
@@ -22,16 +22,21 @@ const Home = () => {
             {searchDisplay ? null : 
             
             <div>
-                    <div className="banner"><h1>Welcome!</h1></div>
+                    {/* <div className="banner"><h1>Welcome!</h1></div> */}
     
                     <div className="homepage-content">
-                        <div>
-                            <img className="homepage-profile-pic-top" src="/img/jimjam3.png" alt="Me" />
+                    
+                        
+                            {/* <img className="homepage-profile-pic-top" src="/img/jimjam3.png" alt="Me" /> */}
                             <p>
-                                <img className="homepage-profile-pic" src="/img/jimjam3.png" alt="Me" />
-                                My name's James Sparrow, I'm a 20 year old former software development student who's passionate
-                                about web development.
-                                Check out my portfolio or blog to see what I've been up to!
+                                {/* <img className="homepage-profile-pic" src="/img/jimjam3.png" alt="Me" /> */}
+                                Hi, I'm James (commonly known as Jimjam). 
+                            </p>
+                            <p>
+                                I'm into web programming (with PHP and JS), making games in Unity (with C#), non-machine people languages (specifically Russian and Mandarin) and Linux/Unix nerd stuff. I mostly post about programming projects here as a record of what I've worked on. 
+                            </p>
+                            <p>
+                                As of September 2020, I study Computer Science at Aston University, Birmingham.
                             </p>
                             <ul className="homepage-quick-links">
                                 <li><Link to="/blog"><i className="fas fa-pen"></i> Blog</Link></li>
@@ -40,8 +45,8 @@ const Home = () => {
                                 <li><Link to="/github"><i className="fab fa-github"></i> Github</Link></li>
                                 <li><Link to="/contact"><i className="fas fa-envelope"></i> Contact</Link></li>
                             </ul>
-                        </div>
-                        <div className="homepage-gallery">
+                        {/* </div> */}
+                        {/* <div className="homepage-gallery">
 
                             <img src="/img/logos/htmlcssjs.png" alt="" />
                             <img src="/img/logos/php.png" alt="" />
@@ -50,7 +55,7 @@ const Home = () => {
                             <img src="/img/logos/c-sharp.png" alt=""/>
                             <img src="/img/logos/unity.png" alt="" />
 
-                        </div>
+                        </div> */}
                     </div>
             </div>
 
